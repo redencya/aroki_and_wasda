acf7daec7b1a3acbe7db4b9be1c5a2597122e8ee
Rose Ilnicka <redencya@gmail.com>
2021-12-11 21:51:34 +0000

    Added functional player character
    This commit introduces a functional player character utilizing 3 scripts:
    - Player.cs, which defines the logic for the player
    - Actor.cs from which the player inherits some basic behavior
    - HoldTime.cs which is used to record the amount of time in which the player is holding a button.

---
+++ Actor.cs
@@ -0,0 +1,99 @@
+using Godot;
+using System;
+using static Actor.MoveType;
+using static Actor.Direction;
+
+
+// The actor is a parent class so it shouldn't have any active parts
+public class Actor : KinematicBody2D
+{
+	public enum Direction
+	{
+		Left = -1,
+		Right = 1,
+	}
+	public enum MoveType
+	{
+		Linear,
+		Exponential
+	}
+
+	public const float gravity = 9.667f * 100;
+
+	public float moveAccel;
+	public float moveDecel;
+
+	public float moveSpeed;
+	public float moveJumpHeight;
+	public Vector2 Velocity = Vector2.Zero;
+
+
+	public Direction InvertDirection(Direction myDir)
+	{
+		return (myDir == Right) ? Left : Right;
+	}
+
+	/// <summary>
+	/// This function is used to implement basic movement.
+	/// The movement can be executed as:
+	/// <list type="bullet">
+	/// <item>
+	/// <term><paramref name="moveType" type=".Linear"/></term>
+	/// <description>Will set actor's velocity to <b>moveSpeed</b> instantly.</description>
+	/// </item>
+	/// <item>
+	/// <term><paramref name="moveType" type=".Exponential"/></term>
+	/// <description>The actor velocity will increase to <b>moveSpeed</b> by <b>moveAccel</b> increments.</description>
+	/// </item>
+	/// </list>
+	/// </summary>
+	/// <param name="myDir"></param>
+	/// <param name="moveType"></param>
+	public void Move(Direction myDir, MoveType moveType = Linear)
+	{
+		switch (moveType)
+		{
+			case Linear:
+				Velocity.x = moveSpeed * (int)myDir;
+				break;
+
+			case Exponential:
+				Velocity.x = (myDir == Right) ?
+					Math.Min(Velocity.x + moveAccel, moveSpeed):
+					Math.Max(Velocity.x - moveAccel, -moveSpeed);
+				break;
+		}
+	}
+
+	public void StopMoving(Direction myDir, MoveType moveType = Linear)
+	{
+		switch (moveType)
+		{
+			case Linear:
+				Velocity.x = 0;
+				break;
+
+			case Exponential:
+				Velocity.x = (myDir == Right) ?
+					Math.Max(Velocity.x - moveDecel, 0) :
+					Math.Min(Velocity.x + moveDecel, 0);
+				break;
+		}
+	}
+
+		public void Jump()
+	{
+		if (IsOnFloor())
+		{
+			Velocity.y = moveJumpHeight;
+		}
+	}
+
+
+	public void ApplyMovement(float delta)
+	{
+		Velocity.y += gravity * delta;
+		Velocity = MoveAndSlide(Velocity, Vector2.Up);
+	}
+
+}
--- Player.cs
+++ Player.cs
@@ -1,21 +1,64 @@
 using Godot;
 using System;
+using static Actor.MoveType;
+using static Actor.Direction;
+using static Player.MoveMode;
 
-public class Player : Node
+public class Player : Actor
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	public enum MoveMode
+	{
+		Regular,
+		Swimming,
+	}
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
+	private Direction currentDirection = Right;
+	private MoveMode currentMoveMode = Regular;
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	// Cache of constants for 
+	private const float airAccel = 10f;
+	private const float groundAccel = 15f;
+	private const float airDecel = 25f;
+	private const float groundDecel = 35f;
+
+	public void DefineMovement(MoveMode myMode = Regular)
+	{
+		moveAccel = (IsOnFloor()) ? groundAccel : airAccel;
+		moveDecel = (IsOnFloor()) ? groundDecel : airDecel;
+		moveSpeed = 2.5f * 100;
+		moveJumpHeight = -5.5f * 100;
+
+		if (HoldTime.isTapped() && !HoldTime.isActive)
+		{
+			currentDirection = InvertDirection(currentDirection);
+		}
+		if (HoldTime.isHeld())
+		{
+			if (HoldTime.isActive)
+			{
+				Move(currentDirection, Exponential);
+			}
+			else
+			{
+				Jump();
+			}
+		}
+		if (!HoldTime.isActive && Velocity.x != 0)
+		{
+			MoveStop(currentDirection, Exponential);
+		}
+	}
+	public override void _PhysicsProcess(float delta)
+	{
+		base._PhysicsProcess(delta);
+		DefineMovement();
+		ApplyMovement(delta);
+		HoldTime.Reset();
+	}
+	public override void _Process(float delta)
+	{
+		base._Process(delta);
+		HoldTime.Calc(delta);
+		GD.Print($"{Velocity.x} {currentDirection}");
+	}
 }
