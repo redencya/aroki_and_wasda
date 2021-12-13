f79c087a0dd6b4ad5d6c307677d8a610a1f0b5e5
Rose Ilnicka <redencya@gmail.com>
2021-12-13 19:37:43 +0000

    Initial commit to working on a state machine based player

---
+++ src/player/PlayerStates.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+using Game.State.Pattern;
+
+namespace Game.State.Concrete
+{
+	public class Idle : IPlayerAction
+	{
+
+	}
+
+	public class Move : IPlayerAction
+	{
+
+	}
+
+	public class Swim : IPlayerAction
+    {
+
+    }
+
+
+}
---
+++ src/player/StatePattern.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using Game;
+
+
+namespace Game.State.Pattern
+{
+    public interface IPlayerAction
+    {
+
+
+    }
+
+}
